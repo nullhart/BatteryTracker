4bd9d710aa45b2533e1369acfaf01f9aab996edc
nullhart <blakejosephmastrud@gmail.com>
2020-05-31 20:07:14 +0000

    Add Update Modal

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,45 @@
 <template>
   <div id="app">
+    <!-- Only shows when update is available -->
+    <div class="modal">
+      <input id="modal_1" type="checkbox" :checked="$store.state.UpdateIsAvailable" />
+      <label for="modal_1" class="overlay"></label>
+      <article>
+        <header style="text-align: center;">
+          <h3 style="margin: 0">Update Available!</h3>
+          <label for="modal_1" class="close">&times;</label>
+        </header>
+        <section
+          style="text-align: center;"
+          class="content"
+        >Do you want to use the updated version now?</section>
+        <footer>
+          <a class="button" @click="reload()">Yes</a>
+          <label for="modal_1" class="button dangerous">Cancel</label>
+        </footer>
+      </article>
+    </div>
+
+    <!-- Main Content -->
     <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  methods: {},
+  methods: {
+    reload: () => {
+      location.reload(true);
+    }
+  },
   async mounted() {}
 };
 </script>
 
 <style lang="scss">
-@import "../node_modules/formbase/dist/formbase.min.css";
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import "./assets/picnic.min.css";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+button {
+  outline: none;
 }
 </style>
