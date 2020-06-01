27a68a4921406c4b0a007bf16ccc1937e4784f3b
nullhart <blakejosephmastrud@gmail.com>
2020-06-01 21:24:37 +0000

    WIP NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="nav-container">
+    <div>home</div>
+    <div>Settings</div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
+
+<style>
+.nav-container {
+  width: 100vw;
+  height: 60px;
+}
+</style>
