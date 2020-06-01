c2bc89e26c591100cc0391169fc2d727e7cc2447
nullhart <blakejosephmastrud@gmail.com>
2020-06-01 21:23:55 +0000

    Create Coordinates Field

---
+++ src/components/Coordinates.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="width: 100%;display: grid;">
+    <label for="lat">Latitude</label>
+    <input name="lat" v-model="lat" />
+
+    <label for="long">Longitude</label>
+    <input name="long" v-model="long" />
+    <button style="margin: auto;margin-top: 10px;" @click="SetLocation">Get Location</button>
+  </div>
+</template>
+
+<script>
+import GetLocation from "../Helpers/GetLocation";
+export default {
+  name: "coordinates",
+  data() {
+    return { lat: "", long: "" };
+  },
+  methods: {
+    SetLocation: async function() {
+      const location = await GetLocation();
+      this.lat = location.coords.latitude;
+      this.long = location.coords.longitude;
+      console.log();
+    }
+  }
+};
+</script>
+
+<style>
+</style>
