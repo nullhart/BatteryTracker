d9d466cd29b269d67e70c5b43cd7ca0186e9dd8f
nullhart <blakejosephmastrud@gmail.com>
2020-06-01 17:27:32 +0000

    Create Number Box Component

---
+++ src/components/NumBox.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <label for="NumBox">{{data.label}}</label>
+    <input
+      type="number"
+      @change="$emit('updateValue', FieldValue)"
+      v-model="FieldValue"
+      :placeholder="data.hint"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NumBox",
+  props: ["data"],
+  data() {
+    return {
+      FieldValue: ""
+    };
+  },
+  computed: {},
+  methods: {},
+  async mounted() {
+    if (this.data.value) {
+      FieldValue = this.data.value;
+    } else if (this.data.default_value) {
+      this.FieldValue = this.data.default_value;
+    } else {
+      this.FieldValue = "";
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
