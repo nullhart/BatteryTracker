08f474bc522909123851bc93b6866f7ef29b3cee
nullhart <blakejosephmastrud@gmail.com>
2020-06-01 17:27:48 +0000

    Create TextBox Component

---
+++ src/components/TextBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label for="TextBox">{{data.label}}</label>
+    <input @change="$emit('updateValue', FieldValue)" v-model="FieldValue" :placeholder="data.hint" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextBox",
+  props: ["data"],
+  data() {
+    return {
+      FieldValue: ""
+    };
+  },
+  computed: {},
+  methods: {},
+  async mounted() {
+    if (this.data.value) {
+      FieldValue = this.data.value;
+    } else if (this.data.default_value) {
+      this.FieldValue = this.data.default_value;
+    } else {
+      this.FieldValue = "";
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
