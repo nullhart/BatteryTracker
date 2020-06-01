b896a557ed9bba8f0e0baeec42b5f8741aa2bd93
nullhart <blakejosephmastrud@gmail.com>
2020-06-01 17:27:41 +0000

    Create TextArea Component

---
+++ src/components/TextArea.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <label for="TextArea">{{data.label}}</label>
+    <textarea
+      @change="$emit('updateValue', FieldValue)"
+      v-model="FieldValue"
+      :placeholder="data.hint"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextArea",
+  props: ["data"],
+  data() {
+    return {
+      FieldValue: ""
+    };
+  },
+  computed: {},
+  methods: {},
+  async mounted() {
+    if (this.data.value) {
+      FieldValue = this.data.value;
+    } else if (this.data.default_value) {
+      this.FieldValue = this.data.default_value;
+    } else {
+      this.FieldValue = "";
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
