a75fe1b399f5c7734a2c8c45481393540eccd7f3
nullhart <blakejosephmastrud@gmail.com>
2020-06-01 21:25:26 +0000

    WIP Local Forms Screen

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list-container">
+    <div class="list-item" v-for="(form,i) in SavedForms" :key="i">{{form.form_name}}</div>
+  </div>
+</template>
+
+<script>
+import GetCreatedForms from "../Helpers/GetCreatedForms";
+export default {
+  name: "Home",
+  data() {
+    return { SavedForms: [] };
+  },
+  async mounted() {
+    this.SavedForms = await GetCreatedForms();
+  }
+};
+</script>
+
+<style>
+.list-container {
+  width: 100vw;
+  height: 100vh;
+}
+
+.list-item {
+  width: 100vw;
+  height: 50px;
+  margin-bottom: 10px;
+  background-color: lightGrey;
+}
+</style>
