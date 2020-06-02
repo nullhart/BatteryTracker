48f7290c9d098cb236f52280527f04f985c9ad36
nullhart <blakejosephmastrud@gmail.com>
2020-06-02 20:47:52 +0000

    add font awesome

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChevronLeft, faSync, faPlus, faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import './registerServiceWorker'
 // import './service-worker'
 import router from './router'
 import store from './store'
 
+
+
+library.add(faChevronLeft, faSync, faPlus, faBars)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 new Vue({
