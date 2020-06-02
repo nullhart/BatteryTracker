3c9b36fe3d8cf3bbcffb807bf08a1db2e3982633
nullhart <blakejosephmastrud@gmail.com>
2020-06-02 20:48:48 +0000

    Add Settings Page

---
+++ src/views/Settings.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="settings-container">
+    <div></div>
+    <button @click="ResetDB">delete Local DB 💣</button>
+  </div>
+</template>
+
+<script>
+import WipeLocalDB from "../Helpers/WipeLocalDB";
+export default {
+  methods: {
+    ResetDB: async function() {
+      this.$store.state.spinner = true;
+
+      await WipeLocalDB();
+      setTimeout(() => {
+        this.$store.state.spinner = false;
+      }, 1000);
+    }
+  }
+};
+</script>
+
+<style>
+.settings-container {
+  display: grid;
+  margin-top: 50px;
+  overflow-y: scroll;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 900px;
+  grid-template-rows: 1fr 60px;
+}
+</style>
