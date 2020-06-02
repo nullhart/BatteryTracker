463506ac329eecd1ef2bc4593c1626f58e5901d8
nullhart <blakejosephmastrud@gmail.com>
2020-06-02 20:45:19 +0000

    Update App Name

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // ...other vue-cli plugin options...
     pwa: {
-        name: 'Battery Tracker',
+        name: 'Mobile Forms V2',
         themeColor: '#212121',
         msTileColor: '#212121',
         appleMobileWebAppCapable: 'yes',
