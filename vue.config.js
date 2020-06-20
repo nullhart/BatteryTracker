f8e3394dacec5777b737d8b3934ccbef928ae135
Blake Mastrud <blakejosephmastrud@gmail.com>
2020-06-20 10:23:55 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
     // ...other vue-cli plugin options...
     pwa: {
         name: 'Mobile Forms V2',
-        themeColor: '#212121',
-        msTileColor: '#212121',
+        themeColor: '#1b2431',
+        msTileColor: '#1b2431',
         appleMobileWebAppCapable: 'yes',
         appleMobileWebAppStatusBarStyle: 'black',
 
